2eb5a5d9f24fb67199fe54802e5558b12f258167
shobiiiii <shobshobe@gmail.com>
2023-05-12 09:44:59 +0330

    adding technical indicators to database and drop NaN rows

--- flask-app.py
+++ flask-app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from functions import fetch_data, plot_data, process_asset_symbol
+import functions
 
 app = Flask(__name__)
 CORS(app)
@@ -16,16 +16,22 @@ def submit():
     end_date = data['endDate']
 
     # Transform the asset name to symbol
-    symbol = process_asset_symbol(asset_type, asset)
+    symbol = functions.process_asset_symbol(asset_type, asset)
 
     # Fetch Data
-    data = fetch_data(symbol, start_date, end_date)
+    data = functions.fetch_data(symbol, start_date, end_date)
 
     # Plot the data
-    plot_file_path = plot_data(symbol, data, start_date, end_date)
+    plot_file_path = functions.plot_data(symbol, data, start_date, end_date)
     image_url = request.url_root + plot_file_path
 
-    return jsonify({'message': 'Data fetched and plotted successfully.', 'image_url': image_url}), 200
+    # Add Technical indicators
+    functions.add_technical_indicators(data, asset_type)
+
+    # Select relevant features
+    features, target = functions.select_features(data, asset_type)
+
+    return jsonify({'message': 'technical added.', 'image_url': image_url}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- functions.py
+++ functions.py
@@ -7,6 +7,10 @@ matplotlib.use('Agg')  # use a non-interactive backend
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+from ta.volatility import AverageTrueRange
+from ta.trend import CCIIndicator
+from ta.momentum import RSIIndicator
+from ta.volume import OnBalanceVolumeIndicator
 
 
 def process_asset_symbol(asset_type, asset):
@@ -22,7 +26,7 @@ def process_asset_symbol(asset_type, asset):
     elif asset_type == 'Forex':
         symbol = asset.replace('/', '') + '=X'
     elif asset_type == 'Stocks':
-        pass  # No transformation needed for stocks
+        symbol = asset  # No transformation needed for stocks
     return symbol
 
 def fetch_data(symbol, start_date, end_date, interval='1d'):
@@ -40,7 +44,8 @@ def fetch_data(symbol, start_date, end_date, interval='1d'):
 
     if os.path.exists(csv_filepath) and os.path.getsize(csv_filepath) > 42:
         print("CSV File Already Exist. Loading data from CSV file.")
-        hist_data = pd.read_csv(csv_filepath)
+        hist_data = pd.read_csv(csv_filepath, parse_dates=[0], index_col=0)
+
     else:
         print("CSV File does not exist. Querying data from yfinance.")
         yf_symbol = yf.Ticker(symbol)
@@ -108,3 +113,83 @@ def save_to_csv(data, symbol, start_date, end_date, interval, dir_name='data'):
     else:
         # Save the data to a CSV file
         data.to_csv(file_path)
+
+def add_technical_indicators(data, asset_type):
+    """
+    Adds technical indicators to a DataFrame of price data.
+
+    :param data: A pandas DataFrame containing price data
+    :param asset_type: A string representing the type of the asset ("stock", "Forex", or "Cryptocurrencies")
+
+    :returns: The input DataFrame with added technical indicators
+    """
+
+    # convert index to a DatetimeIndex
+    data.index = pd.to_datetime(data.index, utc=True)
+
+    # Calculate returns and lagged returns
+    data['Returns'] = data['Close'].pct_change().dropna()
+    data['Lagged_Returns'] = data['Returns'].shift(1).dropna()
+
+    periods = [3, 7, 14, 21]
+
+    for period in periods:
+        # Calculate SMA and EMA
+        data[f'SMA_{period}'] = data['Close'].rolling(window=period).mean()
+        data[f'EMA_{period}'] = data['Close'].ewm(span=period).mean()
+
+        # Calculate RSI
+        rsi = RSIIndicator(data['Close'], window=period).rsi()
+        data[f'RSI_{period}'] = rsi
+
+        # Calculate ATR
+        atr = AverageTrueRange(data['High'], data['Low'], data['Close'], window=period).average_true_range()
+        data[f'ATR_{period}'] = atr
+
+        # Calculate CCI
+        cci = CCIIndicator(data['High'], data['Low'], data['Close'], window=period).cci()
+        data[f'CCI_{period}'] = cci
+
+    # Calculate OBV if asset_type is "stock"
+    if asset_type != "Forex":
+        obv = OnBalanceVolumeIndicator(data['Close'], data['Volume']).on_balance_volume()
+        data[f'OBV'] = obv
+
+        # Calculate OBV_SMA for different periods
+        for period in periods:
+            data[f'OBV_SMA_{period}'] = data['OBV'].rolling(window=period).mean()
+
+    # Drop any NaN rows created due to the indicator calculations
+    data.dropna(inplace=True)
+
+    # Create the categorical features
+    data['day_of_week'] = data.index.dayofweek
+    data['month'] = data.index.month
+    data['quarter'] = data.index.quarter
+
+    # Perform one-hot encoding
+    data = pd.get_dummies(data, columns=['day_of_week', 'month', 'quarter'])
+
+    return data
+
+def select_features(data, asset_type):
+    """
+    Selects the relevant features for the given asset type.
+
+    :param data: A pandas DataFrame containing the data.
+    :param asset_type: A string representing the type of asset ("Forex", "Cryptocurrencies", or "Stocks").
+    :return: A tuple containing the features DataFrame and the target Series.
+    """
+    # Select columns to drop based on the asset type
+    columns_to_drop = ['Returns', 'Lagged_Returns', 'Dividends', 'Stock Splits']
+    if asset_type == "Forex":
+        columns_to_drop.append('Volume')
+
+    # Drop the selected columns from the features
+    features = data.drop(columns=columns_to_drop)
+    print(features.columns)
+
+    # The target is always 'Lagged_Returns'
+    target = data['Lagged_Returns']
+
+    return features, target
